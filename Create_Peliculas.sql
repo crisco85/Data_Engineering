44213c3a083006c9810e0fe05b273b9ee373414a
Cristian_Correa <cristiancorrea85@gmail.com>
2023-09-17 19:52:44 -0300

    Primer subida del proyecto

---
+++ Create_Peliculas.sql
@@ -0,0 +1,9 @@
+create table cristiancorrea85_coderhouse.peliculas(
+	id INT,
+	title VARCHAR(100),
+	overview VARCHAR(300),
+	popularity INT,
+	release_date DATE,
+	vote_average FLOAT,
+	vote_count INT
+)
